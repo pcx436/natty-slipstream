0ae8922646db555b2327112e2956bc4bc5bdd2e0
Jacob Malcy <jmalcy@protonmail.com>
2021-03-01 13:46:21 -0700

    Adding HTTP handler

---
+++ handler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import socketserver
+from http.server import BaseHTTPRequestHandler
+from typing import Tuple
+
+
+class Handler(BaseHTTPRequestHandler):
+	port_num = 3306
+
+	def set_port(self, port_num):
+		self.port_num = port_num
+
+	def _set_headers(self):
+		self.send_response(200)
+		self.send_header('Content-Type', 'text/html')
+		self.end_headers()
+
+	def _html(self):
+		return f'<html><body>{self.port_num}</body></html>'.encode('utf8')
+
+	def do_GET(self):
+		self._set_headers()
+		self.wfile.write(self._html())
+
+	def do_HEAD(self):
+		self._set_headers()
+
+	def do_POST(self):
+		self._set_headers()
+		self.wfile.write(self._html())
